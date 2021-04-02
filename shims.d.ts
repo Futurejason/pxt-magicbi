4680906c14a688d4a0442d0cdd6fac56512dd037
Futurejason <2522680278@qq.com>
2021-04-02 16:15:15 +0800

    3

--- shims.d.ts
+++ shims.d.ts
@@ -2,25 +2,25 @@
 
 
 
-//% color=50 weight=19 icon="\uf1eb"
-//% block="IrRemote"
-declare namespace IrRemote {
+// //% color=50 weight=19 icon="\uf1eb"
+// //% block="IrRemote"
+// declare namespace IrRemote {
 
-    /**
-     * button pushed.
-     */
-    //% blockId=onPressEvent
-    //% block="on |%btn| button pressed" shim=IrRemote::onPressEvent
-    function onPressEvent(btn: remotebutton, body: () => void): void;
+//     /**
+//      * button pushed.
+//      */
+//     //% blockId=onPressEvent
+//     //% block="on |%btn| button pressed" shim=IrRemote::onPressEvent
+//     function onPressEvent(btn: remotebutton, body: () => void): void;
 
-    /**
-     * initialises local variablesssss
-     */
-    //% blockId=IrRemote_init
-    //% block="connect ir receiver to %pin" shim=IrRemote::IrRemote_init
-    function IrRemote_init(pin: Pins): void;
+//     /**
+//      * initialises local variablesssss
+//      */
+//     //% blockId=IrRemote_init
+//     //% block="connect ir receiver to %pin" shim=IrRemote::IrRemote_init
+//     function IrRemote_init(pin: Pins): void;
 
-}
+// }
 
 
 //% color=50 weight=80
