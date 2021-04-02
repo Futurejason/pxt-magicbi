14dbf6b55dea5487e584566b3cef8f3c03647eb3
Futurejason <76203014+Futurejason@users.noreply.github.com>
2021-04-02 14:59:06 +0800

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -40,7 +40,7 @@ declare namespace IrRemote {
     //% weight=5
     //% group="micro:bit(v2)"
     //% blockId=IR_readv2 block="read IR key value"
-    export function IR_readV2(): string {
+    export function IR_readV2(): number {
         return valuotokeyConversion();
     }
 
@@ -55,30 +55,30 @@ declare namespace IrRemote {
         })
     }
 
-    function valuotokeyConversion(): string {
-        let irdata: string;
+    function valuotokeyConversion(): number {
+        let irdata: number;
         switch (irCode()) {
-            case 0xba45: irdata = 'A'; break;
-            case 0xb946: irdata = 'B'; break;
-            case 0xb847: irdata = 'C'; break;
-            case 0xbb44: irdata = 'D'; break;
-            case 0xbf40: irdata = 'UP'; break;
-            case 0xbc43: irdata = '+'; break;
-            case 0xf807: irdata = 'LEFT'; break;
-            case 0xea15: irdata = 'AFFIRM'; break;
-            case 0xf609: irdata = 'RIGHT'; break;
-            case 0xe916: irdata = '0'; break;
-            case 0xe619: irdata = 'DOWN'; break;
-            case 0xf20d: irdata = '-'; break;
-            case 0xf30c: irdata = '1'; break;
-            case 0xe718: irdata = '2'; break;
-            case 0xa15e: irdata = '3'; break;
-            case 0xf708: irdata = '4'; break;
-            case 0xe31c: irdata = '5'; break;
-            case 0xa55a: irdata = '6'; break;
-            case 0xbd42: irdata = '7'; break;
-            case 0xad52: irdata = '8'; break;
-            case 0xb54a: irdata = '9'; break;
+            case 0xba45: irdata = 01; break;
+            case 0xb946: irdata = 02; break;
+            case 0xb847: irdata = 03; break;
+            case 0xbb44: irdata = 04; break;
+            case 0xbf40: irdata = 11; break;
+            case 0xbc43: irdata = 99; break;
+            case 0xf807: irdata = 14; break;
+            case 0xea15: irdata = 00; break;
+            case 0xf609: irdata = 12; break;
+            case 0xe916: irdata = 0; break;
+            case 0xe619: irdata = 13; break;
+            case 0xf20d: irdata = 66; break;
+            case 0xf30c: irdata = 1; break;
+            case 0xe718: irdata = 2; break;
+            case 0xa15e: irdata = 3; break;
+            case 0xf708: irdata = 4; break;
+            case 0xe31c: irdata = 5; break;
+            case 0xa55a: irdata = 6; break;
+            case 0xbd42: irdata = 7; break;
+            case 0xad52: irdata = 8; break;
+            case 0xb54a: irdata = 9; break;
             default:
                 irdata = '-1';
         }
