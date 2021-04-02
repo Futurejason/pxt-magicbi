9c5fa69b1bb8520038ac8564d2565657a010662b
Futurejason <2522680278@qq.com>
2021-04-02 15:44:07 +0800

    2

--- shims.d.ts
+++ shims.d.ts
@@ -20,6 +20,12 @@ declare namespace IrRemote {
     //% block="connect ir receiver to %pin" shim=IrRemote::IrRemote_init
     function IrRemote_init(pin: Pins): void;
 
+}
+
+
+//% color=50 weight=80
+//% icon="\uf1eb"
+declare namespace maqueenIRV2 {
     export class Packeta {
         public mye: string;
         public myparam: number;
@@ -96,5 +102,6 @@ declare namespace IrRemote {
         basic.pause(20);
     })
 }
+
 
 // Auto-generated. Do not edit. Really.
