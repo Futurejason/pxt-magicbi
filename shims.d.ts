856dc4ef62eaadf347b89383240f41f3597bff58
Futurejason <76203014+Futurejason@users.noreply.github.com>
2021-04-07 09:01:52 +0800

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -5,21 +5,6 @@
 //% color=50 weight=19 icon="\uf1eb"
 //% block="magicbit"
 declare namespace magicbit {
-
-    /**
-     * button pushed.
-     */
-    //% blockId=onPressEvent
-    //% block="on |%btn| button pressed" shim=magicbit::onPressEvent
-    function onPressEvent(btn: remotebutton, body: () => void): void;
-
-    /**
-     * initialises local variablesssss
-     */
-    //% blockId=IrRemote_init
-    //% block="connect ir receiver to %pin" shim=magicbit::IrRemote_init
-    function IrRemote_init(pin: Pins): void;
-
 }
 
 //% color=50 weight=80
