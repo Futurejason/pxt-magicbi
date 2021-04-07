59f61ab50be1cb2ec6c440a14d6cb20f805f4fec
Futurejason <76203014+Futurejason@users.noreply.github.com>
2021-04-07 09:09:31 +0800

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -1,16 +1,1 @@
-// Auto-generated. Do not edit.
 
-
-
-//% color=50 weight=19 icon="\uf1eb"
-//% block="magicbit"
-declare namespace magicbit {
-}
-
-//% color=50 weight=80
-//% icon="\uf1eb"
-declare namespace maqueenIRV2 {
-}
-
-
-// Auto-generated. Do not edit. Really.
